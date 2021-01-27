d0183509cb027b2c3f67428f363e1a59413ada7f
Sergei Rudkovskii <rudserg39@gmail.com>
2021-01-27 22:09:59 +0200

    Make corrections

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,6 @@
 // Burger-button
 
-const burger = document.querySelector(".burger");
+var burger = document.querySelector(".burger");
 
 burger.classList.remove("burger_invisible");
 
@@ -16,12 +16,12 @@ burger.addEventListener("keydown", (evt) => {
 
 // Burger-menu
 
-const navPanel = document.querySelector(".header__nav-panel");
+var navPanel = document.querySelector(".header__nav-panel");
 
 navPanel.classList.remove("header__nav-panel_open");
 navPanel.classList.add("header__nav-panel_close");
 
-cons = toggleNavPanel = () => {
+var toggleNavPanel = () => {
   if (navPanel.classList.contains("header__nav-panel_close")) {
     navPanel.classList.remove("header__nav-panel_close");
     navPanel.classList.add("header__nav-panel_open");
@@ -45,49 +45,31 @@ burger.addEventListener("keydown", (evt) => {
 
 // Slider
 
-const slider = document.querySelector(".slider");
-// const sliderImage = slider.querySelector(".slider__img");
-// const beforeButton = slider.querySelector(".slider__before");
-// const afterButton = slider.querySelector(".slider__after");
-// const sliderToggle = slider.querySelector(".slider__toggle-mark");
+var SLIDER_IMG_BEFORE_WIDTH = 677;
+var SLIDER_IMG_AFTER_WIDTH = 545;
+var SCALE_WIDTH = 396;
 
-// beforeButton.addEventListener("click", () => {
-//   sliderImage.src = "./img/slider-before-mobile@1x.png";
-//   sliderImage.srcset = "./img/slider-before-mobile@2x.png 2x";
-//   sliderToggle.style.setProperty("left", "6px");
-//   sliderToggle.style.setProperty("transition-duration", "500ms");
-// });
+var slider = document.querySelector(".slider");
 
-// afterButton.addEventListener("click", () => {
-//   sliderImage.src = "./img/slider-after-mobile@1x.png";
-//   sliderImage.srcset = "./img/slider-after-mobile@2x.png 2x";
-//   sliderToggle.style.setProperty("left", "44px");
-//   sliderToggle.style.setProperty("transition-duration", "500ms");
-// });
+var sliderImgBefore = slider.querySelector(".slider__img_before");
+var sliderImgAfter = slider.querySelector(".slider__img_after");
 
+var scaleMark = slider.querySelector(".slider__scale-mark");
+var sliderToggle = slider.querySelector(".slider__toggle-mark");
 
-const SLIDER_IMG_BEFORE_WIDTH = 677;
-const SLIDER_IMG_AFTER_WIDTH = 545;
-const SCALE_WIDTH = 396;
-
-const sliderImgBefore = slider.querySelector(".slider__img_before");
-const sliderImgAfter = slider.querySelector(".slider__img_after");
-const scaleMark = slider.querySelector(".slider__scale-mark");
-
-const scaleMarkLineBoorders = {
+var scaleMarkLineBoorders = {
   LEFT: 0,
   RIGHT: 396
 };
 
+var widthDifference = (Math.abs((SLIDER_IMG_BEFORE_WIDTH - SLIDER_IMG_AFTER_WIDTH )) / SLIDER_IMG_AFTER_WIDTH ) * 100;
 
-const widthDifference = (Math.abs((SLIDER_IMG_BEFORE_WIDTH - SLIDER_IMG_AFTER_WIDTH )) / SLIDER_IMG_AFTER_WIDTH ) * 100;
-
-const scaleStep = 396 / 100;
+var scaleStep = 396 / 100;
 
 
-scaleMark.addEventListener('mousedown', function (evt) {
+scaleMark.addEventListener("mousedown", function (evt) {
   evt.preventDefault();
-
+  
   var startCoords = {
     x: evt.clientX
   };
@@ -103,29 +85,27 @@ scaleMark.addEventListener('mousedown', function (evt) {
       x: moveEvt.clientX
     };
 
+    scaleMark.style.left = scaleMark.offsetLeft + shift.x + "px";
 
-    scaleMark.style.left = scaleMark.offsetLeft + shift.x + 'px';
-
-    
     if (scaleMark.offsetLeft < scaleMarkLineBoorders.LEFT) {
-      scaleMark.style.left = scaleMarkLineBoorders.LEFT + 'px';
+      scaleMark.style.left = scaleMarkLineBoorders.LEFT + "px";
     }
 
     if (scaleMark.offsetLeft > scaleMarkLineBoorders.RIGHT) {
-      scaleMark.style.left = scaleMarkLineBoorders.RIGHT + 'px';
+      scaleMark.style.left = scaleMarkLineBoorders.RIGHT + "px";
     }
 
-    sliderImgBefore.style.width = 100 - (scaleMark.offsetLeft / 100) / scaleStep * 100 + '%';
-    sliderImgAfter.style.width = (scaleMark.offsetLeft / 100) / scaleStep * 100 - widthDifference + '%';
+    sliderImgBefore.style.width = 100 - (scaleMark.offsetLeft / 100) / scaleStep * 100 + "%";
+    sliderImgAfter.style.width = (scaleMark.offsetLeft / 100) / scaleStep * 100 - widthDifference + "%";
   };
 
   var mouseUpHandler = function (upEvt) {
     upEvt.preventDefault();
 
-    document.removeEventListener('mousemove', mouseMoveHandler);
-    document.removeEventListener('mouseup', mouseUpHandler);
+    document.removeEventListener("mousemove", mouseMoveHandler);
+    document.removeEventListener("mouseup", mouseUpHandler);
   };
 
-  document.addEventListener('mousemove', mouseMoveHandler);
-  document.addEventListener('mouseup', mouseUpHandler);
+  document.addEventListener("mousemove", mouseMoveHandler);
+  document.addEventListener("mouseup", mouseUpHandler);
 });
